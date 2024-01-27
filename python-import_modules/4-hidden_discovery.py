61e27aa8887ceec9667bf011037f834a27f62123
root <gqasimova93@gmail.com>
2024-01-27 10:32:01 -0800

    fixed false error

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import  hidden_4
+    import hidden_4
     string = dir(hidden_4)
     for i in string:
-        if i.startswith("__") == False:
+        if not i.startswith("__"):
             print("{}".format(i))
