c0db7ae9d7424dbc1874597cd9ac8c1d5ad6a366
root <gqasimova93@gmail.com>
2024-01-27 15:43:09 -0800

    adds 2 tuples.

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-     tuple_0 = (0, )
+    tuple_0 = (0, )
     for i in range(len(tuple_a), 2):
         tuple_a += tuple_0
     for i in range(len(tuple_b), 2):
