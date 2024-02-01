ee391c818522f15e3106b52ae3407cb419011740
root <gqasimova93@gmail.com>
2024-02-01 07:32:14 -0800

    pycodestyle fixed

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    matrix_list = list(map(lambda x: list(map(lambda y: y**2,x)), matrix))
-    return(matrix_list)
+    matrix_list = list(map(lambda x: list(map(lambda y: y**2, x)), matrix))
+    return (matrix_list)
