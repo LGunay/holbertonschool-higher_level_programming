60e141278ade123c290a963ff5108072b631a57a
root <gqasimova93@gmail.com>
2024-02-04 07:13:59 -0800

    divides element by element 2 lists

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -14,5 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             res = 0
         finally:
-            new_list.append(div)
+            new_list.append(res)
     return new_list
