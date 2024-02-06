89dc5d8c8a6916f5f5e498ddf3c42a45fd81db02
root <gqasimova93@gmail.com>
2024-02-06 08:42:20 -0800

    fixed pycodestyle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,31 +4,32 @@
 
 class Rectangle:
     """Rectangle"""
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width=0, height=0):
         """defined Rectangle"""
         self.width = width
         self.height = height
-        
+
     @property
     def width(self):
         """private value of width"""
         return self.__width
+
     @width.setter
     def width(self, value):
-        #set width
         if isinstance(value, int):
             self.__width = value
         else:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+
     @property
     def height(self):
         """private value of height"""
         return self.__height
+
     @height.setter
     def height(self, value):
-        #set height
         if isinstance(value, int):
             self.__height = value
         else:
