92ddff42e1f8d024dbbaa1dcf6bee193247490d4
root <gqasimova93@gmail.com>
2024-02-09 10:13:08 -0800

    Only sub class of

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     '''define function'''
-    return type(obj) is not class and isinstance(obj, a_class)
+    return type(obj) is not a_class and isinstance(obj, a_class)
