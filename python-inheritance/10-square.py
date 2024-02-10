7522e166aef712f33d51796da02b6aee6b9450b6
root <gqasimova93@gmail.com>
2024-02-10 07:44:41 -0800

    Square

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square class that inherits from Rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square"""
+    def __init__(self, size):
+        self.__size = size
+        self.inreger_validator("size", size)
+        super().__init__(size, size)
