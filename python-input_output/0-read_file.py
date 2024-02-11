4f55b7ff4be1653961879204c35f48126e283583
root <gqasimova93@gmail.com>
2024-02-11 00:46:25 -0800

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 '''Read files'''
 
 
 def read_file(filename=""):
+    '''define function'''
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
         f.close()
