11ade23b96e9f44d9fff71732e89011f136f4d9b
root <gqasimova93@gmail.com>
2024-02-11 02:12:30 -0800

    input ouput

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''Student to JSON'''
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+     def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            res = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    res[i] = self.__dict__[i]
+            return res
+
+    def reload_from_json(self, json):
+        if not json:
+            return
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
