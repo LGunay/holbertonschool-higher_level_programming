684ffe5f2cc041b85d680db21c1edfb41857567c
root <gqasimova93@gmail.com>
2024-02-12 07:41:58 -0800

    getter setter

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -11,3 +11,55 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
+            self.__width = value
+        else:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int):
+            self.__height = value
+        else:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @x.setter
+    def x(self, value):
+        if isinstance(value, int):
+            self.__x = value
+        else:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @y.setter
+    def y(self, value):
+        if isinstance(value, int):
+            self.__y = value
+        else:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
